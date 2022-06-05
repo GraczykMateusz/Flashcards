a4b7c345fe29def453e47c79576e8f236995b06c
GraczykMateusz <dev.graczykmateusz@gmail.com>
2022-06-05 19:01:25 +0200

    added dashboard draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'astral';
+
+  year = '2022';
+  build = '0.0';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FlexModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
