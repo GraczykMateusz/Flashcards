36c8aff4bb83fed8b895f51c9636164c47566e09
GraczykMateusz <dev.graczykmateusz@gmail.com>
2022-07-23 16:09:28 +0200

    background better style + refactor

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,40 +1,13 @@
-import {Component, EventEmitter, OnDestroy, OnInit, Output, Renderer2} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnDestroy {
+export class DashboardComponent {
 
-  @Output() blurEvent = new EventEmitter<boolean>();
-  isBlurred = false;
-  audioPath = '../../../assets/audio/the-real-north-intro.mp3';
-
-  constructor(private renderer: Renderer2,
-              private router: Router) {
+  constructor() {
   }
 
-  ngOnDestroy(): void {
-    this.renderer.removeStyle(document.body, 'filter');
-  }
-
-  async blur(): Promise<void> {
-    this.isBlurred = !this.isBlurred;
-
-    this.isBlurred ? this.renderer.setStyle(document.body, 'filter', "blur(4em)")
-      : this.renderer.removeStyle(document.body, 'filter');
-
-    await new Promise(f => setTimeout(f, 3000));
-
-    this.router.navigate(['/flashcards']).then();
-  }
-
-  playAudio(): void {
-    const audio = new Audio();
-    audio.src = this.audioPath;
-    audio.load();
-    audio.play();
-  }
 }
