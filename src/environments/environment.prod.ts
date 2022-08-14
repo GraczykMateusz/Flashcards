723ff97285780ec997835fa7b4afe42b8e966550
GraczykMateusz <dev.graczykmateusz@gmail.com>
2022-08-14 10:20:32 +0200

    added env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'astral-92b54',
+    appId: '1:533455434240:web:f9e290c5257880dee3dee5',
+    storageBucket: 'astral-92b54.appspot.com',
+    locationId: 'europe-west',
+    apiKey: '',
+    authDomain: 'astral-92b54.firebaseapp.com',
+    messagingSenderId: '533455434240',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'astral-92b54',
+    appId: '1:533455434240:web:f9e290c5257880dee3dee5',
+    storageBucket: 'astral-92b54.appspot.com',
+    locationId: 'europe-west',
+    apiKey: '',
+    authDomain: 'astral-92b54.firebaseapp.com',
+    messagingSenderId: '533455434240',
+  }
 };
 
 /*
