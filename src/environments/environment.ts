38be5f46c04b6aa454cf99f65b12513149b6d9b8
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2022-08-26 19:07:19 +0200

    REVERT + circleci

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {FIREBASE_API_DEV} from "../config/keys";
+
 export const environment = {
   production: false,
-  firebase: {
-    projectId: 'astral-92b54',
-    appId: '1:533455434240:web:f9e290c5257880dee3dee5',
-    storageBucket: 'astral-92b54.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'FIREBASE_API_KEY_FROM_CIRCLECI',
-    authDomain: 'astral-92b54.firebaseapp.com',
-    messagingSenderId: '533455434240',
-  }
+  firebase: FIREBASE_API_DEV
 };
 
 /*
