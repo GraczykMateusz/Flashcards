268c0cdbade3e782350ab9eb3e177a9d12686bd1
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2022-11-13 15:55:01 +0100

    fixing

---
+++ src/app/components/flashcards/flashcards-editor/add-flashcard-dialog/add-flashcard-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-flashcard-dialog',
+  templateUrl: './add-flashcard-dialog.component.html',
+  styleUrls: ['./add-flashcard-dialog.component.scss']
+})
+export class AddFlashcardDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
